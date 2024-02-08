8d9b4bfdc16dbeb2341253173226e922cd25f2fa
Tomasz Leś <szkolenia@tomaszles.pl>
2024-02-08 10:59:04 +0100

    q

---
+++ P06Zawodnicy.Shared/Domain/Trener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04Zawodnicy.Shared.Domain
+{
+    public class Trener
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        public string PelneImie => $"{Imie} {Nazwisko}";
+    }
+}
---
+++ P06Zawodnicy.Shared/Domain/Zawodnik.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04Zawodnicy.Shared.Domain
+{
+    public class Zawodnik
+    {
+        public int Id_zawodnika { get; set; }
+        public int? Id_trenera { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Kraj { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+        public int Wzrost { get; set; }
+        public int Waga { get; set; }
+
+        public string ImieNazwisko => Imie + " " + Nazwisko;
+
+        public static string[] WyswietlaneKolumny { get; set; }   // np: {Imie, Nazwisko, Kraj} 
+
+        public object this[string nazwaWlasciwosci]
+        {
+            get
+            {
+                return this.GetType().GetProperty(nazwaWlasciwosci).GetValue(this, null);
+            }
+            set
+            {
+                this.GetType().GetProperty(nazwaWlasciwosci).SetValue(this, value, null);
+            }
+        }
+
+        public string DynamicznaWlasciwosc
+        {
+            get
+            {
+                string s = "";
+                foreach (var k in WyswietlaneKolumny)
+                    s += this[k] + " ";
+                
+                return s;
+            }
+        }
+    }
+}
+
+
