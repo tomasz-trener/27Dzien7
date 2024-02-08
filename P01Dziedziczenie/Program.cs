f3d266d012546b3790364703e7acdb7896d2ab8c
Tomasz Leś <szkolenia@tomaszles.pl>
2024-02-08 08:38:08 +0100

    Add project files.

---
+++ P01Dziedziczenie/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01Dziedziczenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Programista p = new Programista();
+            p.Imie = "Jan";
+            p.Nazwisko = "Kowalski";
+            p.TypKomputera = "Laptop";
+            p.Programuj();
+
+            var z = new Zawodnik();
+            z.Imie = "Adam";
+            z.Nazwisko = "Nowak";
+            z.NrNarty = 42;
+            z.Skocz();
+
+            Czlowiek c1 = new Czlowiek();
+
+            Czlowiek c2 = (Czlowiek)new Zawodnik(); // typ szczegółowy na bardziej ogólny nie wymaga jawnego rzutowania 
+
+            c2.Imie = "Jan";
+            
+            ((Zawodnik)c2).Skocz(); // na czas wywoływania metody "skocz" tratkuje c2 jako zawodnika 
+
+            List<Zawodnik> tylkoZawodnicy = new List<Zawodnik>();
+            List<Programista> tylkoProgramisci = new List<Programista>();
+            // zamiast list indywidualnych...
+
+            List<Czlowiek> osoby = new List<Czlowiek>();
+            osoby.Add(p);
+            osoby.Add(z);
+
+            foreach (var o in osoby)
+            {
+               // Console.WriteLine(o.Imie + " " + o.Nazwisko + " " + ((Zawodnik)o).NrNarty);
+
+                if (o is Zawodnik)
+                    Console.WriteLine(o.Imie + " " + o.Nazwisko + " " + ((Zawodnik)o).NrNarty);
+                else if (o is Programista)
+                    Console.WriteLine(o.Imie + " " + o.Nazwisko + " " + ((Programista)o).TypKomputera);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P01Dziedziczenie/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("P01Dziedziczenie")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("P01Dziedziczenie")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f8115b2-8ee4-4c4b-901e-069dad17bcd1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
